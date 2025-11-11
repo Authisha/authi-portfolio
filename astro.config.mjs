9e0390e2a49c6bfa0663af8ca098628b7887269e
Authisha <authisha.thirumani_selvam@students.iiserpune.ac.in>
2025-11-12 03:29:51 +0530

    Configure sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://authisha-site.vercel.app', // your deployed site URL
   integrations: [
     mdx(),
     tailwind(),
-    sitemap(), // only one sitemap integration
+    sitemap(), // generates sitemap.xml automatically
   ],
 });
